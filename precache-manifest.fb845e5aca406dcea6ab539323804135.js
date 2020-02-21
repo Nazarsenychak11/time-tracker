c3650f99862c618172f607cfcafe81fa3b524d06
Nazarsenychak <nazaesenychak1@gmail.com>
2020-02-21 13:09:13 +0200

    Updates

---
+++ precache-manifest.fb845e5aca406dcea6ab539323804135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5991a7ff1a0e421d3752cd33d9db09b1",
+    "url": "/time-tracker/index.html"
+  },
+  {
+    "revision": "25a2e6585a440a93505a",
+    "url": "/time-tracker/static/css/main.eb0251f2.chunk.css"
+  },
+  {
+    "revision": "6b2c8116921bb81734bb",
+    "url": "/time-tracker/static/js/2.e43e813a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/time-tracker/static/js/2.e43e813a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a2e6585a440a93505a",
+    "url": "/time-tracker/static/js/main.0e487e7b.chunk.js"
+  },
+  {
+    "revision": "10948de30e24101d6abc",
+    "url": "/time-tracker/static/js/runtime-main.b71e49fd.js"
+  }
+]);
